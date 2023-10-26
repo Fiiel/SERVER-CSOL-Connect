8c64bf187f6b59a9217720059dbade6a24b3a2d5
Sh1aze <officialshintv@gmail.com>
2023-10-26 19:29:27 +0800

    added fresh install feat

--- CSOL Connect Server App/Program.cs
+++ CSOL Connect Server App/Program.cs
@@ -1,3 +1,5 @@
+using System.Data.SqlClient;
+
 namespace CSOL_Connect_Server_App
 {
     internal static class Program
@@ -11,7 +13,35 @@ namespace CSOL_Connect_Server_App
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Admin_Dashboard());
+
+            try
+            {
+                // Establish a database connection
+                using (SqlConnection connection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=CSOLconnect;Integrated Security=True"))
+                {
+                    connection.Open();
+
+                    // Create a SQL command to count the number of super admin users
+                    string query = "SELECT COUNT(*) FROM Users WHERE [User Level] = 'Super Admin'";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    int superAdminCount = Convert.ToInt32(command.ExecuteScalar());
+
+                    if (superAdminCount <= 0)
+                    {
+                        Application.Run(new SuperAdmin_AddUsers());
+                    }
+                    else
+                    {
+                        Application.Run(new LoginForm());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error with connecting to the database: " + ex.Message);
+            }
+
         }
     }
 }
