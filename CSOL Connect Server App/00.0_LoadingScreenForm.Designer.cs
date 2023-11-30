5836641bfa6a50668121b73488c5aa4a5b786ef1
Fiiel <neildavid618@gmail.com>
2023-11-30 09:19:48 +0800

    added some design in loading screenn

--- CSOL Connect Server App/00.0_LoadingScreenForm.Designer.cs
+++ CSOL Connect Server App/00.0_LoadingScreenForm.Designer.cs
@@ -32,6 +32,8 @@
             Label_CSOLConnect = new Label();
             PictureBox_CSOLbldg = new PictureBox();
             Label_Initializing = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)PictureBox_CSOLbldg).BeginInit();
             SuspendLayout();
@@ -46,6 +48,7 @@
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
+            pictureBox1.UseWaitCursor = true;
             // 
             // Label_CSOLConnect
             // 
@@ -57,6 +60,7 @@
             Label_CSOLConnect.Size = new Size(152, 30);
             Label_CSOLConnect.TabIndex = 21;
             Label_CSOLConnect.Text = "CSOL Connect";
+            Label_CSOLConnect.UseWaitCursor = true;
             // 
             // PictureBox_CSOLbldg
             // 
@@ -69,17 +73,42 @@
             PictureBox_CSOLbldg.SizeMode = PictureBoxSizeMode.StretchImage;
             PictureBox_CSOLbldg.TabIndex = 22;
             PictureBox_CSOLbldg.TabStop = false;
+            PictureBox_CSOLbldg.UseWaitCursor = true;
             // 
             // Label_Initializing
             // 
             Label_Initializing.AutoSize = true;
+            Label_Initializing.BackColor = Color.FromArgb(7, 25, 82);
             Label_Initializing.Font = new Font("Segoe UI", 12F, FontStyle.Italic, GraphicsUnit.Point);
-            Label_Initializing.ForeColor = Color.FromArgb(7, 25, 82);
+            Label_Initializing.ForeColor = Color.FromArgb(255, 255, 192);
             Label_Initializing.Location = new Point(12, 136);
             Label_Initializing.Name = "Label_Initializing";
             Label_Initializing.Size = new Size(91, 21);
             Label_Initializing.TabIndex = 23;
             Label_Initializing.Text = "Initializing...";
+            Label_Initializing.UseWaitCursor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(7, 25, 82);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 1.5F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(-5, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(431, 3);
+            textBox1.TabIndex = 24;
+            textBox1.UseWaitCursor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(7, 25, 82);
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(-5, 93);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(431, 128);
+            textBox2.TabIndex = 25;
+            textBox2.UseWaitCursor = true;
             // 
             // LoadingScreenForm
             // 
@@ -88,14 +117,17 @@
             BackColor = Color.FromArgb(255, 255, 192);
             ClientSize = new Size(420, 175);
             ControlBox = false;
-            Controls.Add(Label_Initializing);
             Controls.Add(PictureBox_CSOLbldg);
+            Controls.Add(textBox1);
+            Controls.Add(Label_Initializing);
             Controls.Add(Label_CSOLConnect);
             Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             MaximizeBox = false;
             Name = "LoadingScreenForm";
             StartPosition = FormStartPosition.CenterScreen;
+            UseWaitCursor = true;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)PictureBox_CSOLbldg).EndInit();
             ResumeLayout(false);
@@ -107,5 +139,7 @@
         private Label Label_CSOLConnect;
         private PictureBox PictureBox_CSOLbldg;
         private Label Label_Initializing;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
