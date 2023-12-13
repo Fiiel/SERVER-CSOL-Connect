409af3730d16b0b1d6e74ed8d98c3b798ec2e1a3
Fiiel <neildavid618@gmail.com>
2023-12-13 09:10:42 +0800

    fixed form loading

--- CSOL Connect Server App/Program.cs
+++ CSOL Connect Server App/Program.cs
@@ -13,7 +13,7 @@ namespace CSOL_Connect_Server_App
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new SuperAdmin_Dashboard());
+            Application.Run(new LoadingScreenForm());
         }
     }
 }
